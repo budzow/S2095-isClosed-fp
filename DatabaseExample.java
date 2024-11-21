7f93ca76deac334cbe69f4fe1e9aca17461aa268
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2024-11-21 09:02:23 +0100

    simplified to only include connection object

--- DatabaseExample.java
+++ DatabaseExample.java
@@ -1,19 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseExample {
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:your_database_url", "username", "password");
-        PreparedStatement ps = null;
+        Connection connection = null;
+        
         try {
-
-            ps = connection.prepareStatement("SELECT * FROM DUAL");
+			connection = DriverManager.getConnection("jdbc:your_database_url");
+            
         } finally {
-            if (ps != null && !ps.isClosed()) { // FP raised
-                ps.close();
+			if (connection != null && !connection.isClosed()) {
+                connection.close();
             }
         }
 
